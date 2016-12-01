109c3d3c945d88af1cff678b32f54fa68eb44233
Jordan Hall <M0009597@windsor-life.co.uk>
2016-12-01 11:43:52 +0000

    Requires class for typescript to work

--- ESNotify.d.ts
+++ ESNotify.d.ts
@@ -1,4 +1,4 @@
-export interface ESNotify {
+export class ESNotify {
     info(message: string, time: number): void;
 		warning(message: string, time: number): void;
 		error(message: string, time: number): void;
